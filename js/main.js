27059e25b00de571d0c68c8f4e9380a2483fc1c0
hugolalouve <htc.leloup@gmail.com>
2018-05-28 22:36:32 +0200

    Managed to get a JSON, yay!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 window.onload = store;
 
+var OMDB = "http://www.omdbapi.com/?apikey=f4982a00&s=";
+var kiss = true;
+
 function store()
 {
    document.getElementById('searchButton').onclick = function()
    {
       document.getElementById('movieSearch')
-       console.log("http://www.omdbapi.com/?apikey=f4982a00&s=" + movieSearch.value);
-   }
+        // console.log(OMDB + movieSearch.value);
+        fetch(OMDB + movieSearch.value)
+          .then(res => res.json())
+          .then(posts => console.log(posts))
+    }
 }
