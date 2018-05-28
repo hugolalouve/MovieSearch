b4c24408ebb0aa5b6a41c946432c1abb1b8f1e5c
hugolalouve <htc.leloup@gmail.com>
2018-05-28 07:05:45 +0200

    kk

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ function store()
    document.getElementById('searchButton').onclick = function()
    {
       document.getElementById('movieSearch')
-       console.log(movieSearch.value);
+       console.log("http://www.omdbapi.com/?apikey=f4982a00&s=" + movieSearch.value);
    }
 }
